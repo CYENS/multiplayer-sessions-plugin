e56079c3e8d7af937086529f77279613879a6469
Chrysostomos Chadjiminas <chadjiminas.chrysostomos@gmail.com>
2025-09-01 16:02:45 +0300

    feat: Add MultiplayerSessionsLibrary function library for ServerTravel BP nodes

---
+++ Source/MultiplayerSessions/Private/MultiplayerSessionsLibrary.cpp
@@ -0,0 +1,54 @@
+﻿
+#include "MultiplayerSessionsLibrary.h"
+
+bool UMultiplayerSessionsLibrary::ServerTravel(UObject* WorldContextObject, const FString& FURL, const bool bAbsolute, const bool bShouldSkipGameNotify)
+{
+	if (!WorldContextObject)
+	{
+		return false;
+	}
+
+	UWorld* const World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::ReturnNull);
+	if (!World)
+	{
+		return false;
+	}
+	
+	return World->ServerTravel(FURL, bAbsolute, bShouldSkipGameNotify);
+}
+
+bool UMultiplayerSessionsLibrary::ServerTravelToLevel(UObject* WorldContextObject, const TSoftObjectPtr<UWorld> LobbyServerTravelMap, const bool bAbsolute, const bool bShouldSkipGameNotify)
+{
+	if (LobbyServerTravelMap.IsNull())
+	{
+		return false;
+	}
+	
+	const FString Url =  GetServerTravelLobbyUrl(LobbyServerTravelMap);
+	return ServerTravel(WorldContextObject, Url, bAbsolute, bShouldSkipGameNotify);
+}
+
+FString UMultiplayerSessionsLibrary::GetServerTravelLobbyUrl(const TSoftObjectPtr<UWorld>& LobbyServerTravelMap)
+{
+	FString ServerTravelLobbyMapPath;
+	if (!LobbyServerTravelMap.IsNull())
+	{
+		// CanonicalAssetPath is something like "/Game/ThirdPerson/Maps/LobbyMap.LobbyMap"
+		const FString CanonicalAssetPath = LobbyServerTravelMap.ToString();
+		// Remove the ".*" part and add "?listen" to the end
+		if (
+			const int32 DotPosition = CanonicalAssetPath.Find(".");
+			DotPosition != INDEX_NONE
+		)
+		{
+			ServerTravelLobbyMapPath = CanonicalAssetPath.Left(DotPosition) + "?listen";
+		}
+	}
+	else
+	{
+		const FString DefaultLobbyMapPath { "/Game/ThirdPerson/Maps/LobbyMap?listen" };
+		ServerTravelLobbyMapPath = DefaultLobbyMapPath;
+	}
+	return ServerTravelLobbyMapPath;
+}
+
---
+++ Source/MultiplayerSessions/Public/MultiplayerSessionsLibrary.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MultiplayerSessionsLibrary.generated.h"
+
+UCLASS()
+class MULTIPLAYERSESSIONS_API UMultiplayerSessionsLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Online|AdvancedSessions|Seamless", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject"))
+	static bool ServerTravel(UObject* WorldContextObject, const FString& InURL, bool bAbsolute, bool bShouldSkipGameNotify);
+	
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Online|AdvancedSessions|Seamless", meta = (HidePin = "WorldContextObject", DefaultToSelf = "WorldContextObject"))
+	static bool ServerTravelToLevel(UObject* WorldContextObject, const TSoftObjectPtr<UWorld> LobbyServerTravelMap, bool bAbsolute, bool bShouldSkipGameNotify);
+
+	static FString GetServerTravelLobbyUrl(const TSoftObjectPtr<UWorld>& LobbyServerTravelMap);
+};
